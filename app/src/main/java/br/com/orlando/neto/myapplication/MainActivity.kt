6d9d8457fa89775e3d31dd0be7b3644e5335f243
orlando <orlando.neto@maximatech.com.br>
2022-06-23 22:17:48 -0300

    Create Project

---
+++ app/src/main/java/br/com/orlando/neto/myapplication/MainActivity.kt
@@ -0,0 +1,45 @@
+package br.com.orlando.neto.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.orlando.neto.myapplication.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    CreateBizCard()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CreateBizCard() {
+    androidx.compose.material.Surface(modifier = Modifier
+        .fillMaxWidth()
+        .fillMaxWidth()) {
+    
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyApplicationTheme {
+        CreateBizCard()
+    }
+}
